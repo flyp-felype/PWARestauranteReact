26f0939dac8fd7de2fd42c536583bd0fd26dda1c
Felype Gustavo <felypeweb@gmail.com>
2018-06-11 16:47:04 -0300

    ignorando o firebase.js

--- project/utils/firebase.js
+++ project/utils/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
+    apiKey: "AIzaSyA3_J-zgYMD8Y63vDLSNVwSj0NQOMFvVJk",
+    authDomain: "restaurant-a454f.firebaseapp.com",
+    databaseURL: "https://restaurant-a454f.firebaseio.com",
+    projectId: "restaurant-a454f",
     storageBucket: "",
-    messagingSenderId: ""
+    messagingSenderId: "922584690475"
 };
 
 export const firebaseImpl = firebase.initializeApp(config);
