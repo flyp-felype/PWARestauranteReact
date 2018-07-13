9a23d70fae218c739cc13fc0c3956c2c91b11401
Felype Gustavo <felypeweb@gmail.com>
2018-07-13 16:02:48 -0300

    firebase

--- project/utils/firebase.js
+++ project/utils/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyA3_J-zgYMD8Y63vDLSNVwSj0NQOMFvVJk",
-    authDomain: "restaurant-a454f.firebaseapp.com",
-    databaseURL: "https://restaurant-a454f.firebaseio.com",
-    projectId: "restaurant-a454f",
+    apiKey: "API-KEY",
+    authDomain: "DOMAIN",
+    databaseURL: "DATABASE",
+    projectId: "PROJECTID",
     storageBucket: "",
-    messagingSenderId: "922584690475"
+    messagingSenderId: "MENSAGEM"
 };
 
 export const firebaseImpl = firebase.initializeApp(config);
